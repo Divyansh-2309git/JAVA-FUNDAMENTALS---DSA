9a9d3210e77661a9eddbc7081ca451f6f9cb099e
Divyansh <gajendrasingsagar01011985@gmail.com>
2026-06-12 21:37:43 +0530

    Started Queue

---
+++ Leetcode/solution20.java
@@ -0,0 +1,38 @@
+
+import java.util.Stack;
+
+public class solution20{
+    public static boolean isValid(String s ){
+        Stack <Character> st = new Stack<>(); 
+        for (int i = 0 ; i < s.length() ; i ++ ) {
+            char ch = s.charAt(i) ; 
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch); 
+            }else{
+                if(st.isEmpty()){
+                    return false ; 
+                }
+                if(ch == st.peek()){
+                    st.pop(); 
+                }
+                if((st.peek() == '(' && ch == ')' )
+                    || (st.peek() == '{' && ch == '}')
+                    || (st.peek() == '[' && ch == ']')){
+                        st.pop(); 
+                    }else{
+                        return  false ; 
+                    }
+            }          
+        }
+        if(st.isEmpty()){
+            return true ; 
+        }else{
+            return false ; 
+        }
+    }
+    public static void main(String[] args) {
+        String s = "()" ; 
+        System.out.println(isValid(s)) ;
+    }
+}
---
+++ Queue/queueUsingTwoStacks.java
@@ -0,0 +1,6 @@
+public class queueUsingTwoStacks{
+    
+    public static void main(String[] args) {
+        
+    }
+}
