dd787d0df63170ad900baea4690f5918e4fc822f
Divyansh <gajendrasingsagar01011985@gmail.com>
2026-06-14 18:40:50 +0530

    queues midway done

--- Queue/queueUsingTwoStacks.java
+++ Queue/queueUsingTwoStacks.java
@@ -1,6 +1,48 @@
+
+import java.util.*;
+
+
 public class queueUsingTwoStacks{
     
-    public static void main(String[] args) {
+    static class Queue{
+        static Stack<Integer> s1 = new Stack<>(); 
+        static Stack<Integer> s2 = new Stack<>(); 
+
+        public static boolean isEmpty(){
+            return s1.isEmpty(); 
+        }
+
+        public static void add(int data ){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop()); 
+            }
+
+            s1.push(data); 
+
+            while(!s2.isEmpty()){
+                s1.push(s2.pop()); 
+            }
+        }
+
+        public static int remove(){
+            if(s1.isEmpty()){
+                System.out.println("The queue is empty. "); 
+                return -1 ; 
+            }
+            return s1.pop(); 
+
+        }
         
+        public static int peek(){
+            if(s1.isEmpty()){
+                System.out.println("The queue is empty.") ; 
+                return -1 ; 
+            }
+            return s1.peek(); 
+        }
+    }
+
+    public static void main(String[] args) {
+                
     }
 }
