b3bcb6966e08b18b24e5879ffcaf12ce5abd76a4
Divyansh <gajendrasingsagar01011985@gmail.com>
2026-06-15 23:03:11 +0530

    queue questions

---
+++ Practice/Arrays/monotonicArrayList.java
@@ -0,0 +1,11 @@
+public class monotonicArrayList{
+    public static boolean monotonic(int[] nums){
+    
+
+    
+    }
+    public static void main(String[] args) {
+        int[] nums = {1 , 2 , 2 , 3} ; 
+        System.out.println(monotonic(nums)) ; 
+    }
+}
